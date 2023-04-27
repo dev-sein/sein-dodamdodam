bd6081693f2da0b4b25ecf34e628fae66befc6e4
dev-luk <rladnrtjdsla@gmail.com>
2023-04-28 07:59:21 +0900

    mypage, sell 작업

--- src/main/resources/static/js/calendar/calendar.js
+++ src/main/resources/static/js/calendar/calendar.js
@@ -1,107 +1,154 @@
-(function(){
-    $(function(){
-      // calendar element 취득
-      var calendarEl = $('#calendar')[0];
-      // full-calendar 생성하기
-      var calendar = new FullCalendar.Calendar(calendarEl, {
-        height: '700px', // calendar 높이 설정
-        expandRows: true, // 화면에 맞게 높이 재설정
-        slotMinTime: '08:00', // Day 캘린더에서 시작 시간
-        slotMaxTime: '20:00', // Day 캘린더에서 종료 시간
-        // 해더에 표시할 툴바
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+/* -------- 캘린더 시작 ----------*/
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+
+initialView: 'dayGridMonth',
+selectable: true,// 달력 일자 드래그 설정가능
+navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
+editable: true, // 수정 가능
+nowIndicator: true, // 현재 시간 마크
+dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+locale: 'ko', // 한국어 설정
+eventAdd: function(obj) { // 이벤트가 추가되면 발생하는 이벤트
+  console.log('add');
+  
+},
+eventChange: function(obj) { // 이벤트가 수정되면 발생하는 이벤트
+  
+  // GMT 시간은 9시간 빨라서 9시간 빼줘야됨
+  var start = obj.event._instance.range.start;
+  if(start.getHours() == 9) {
+    start = moment(start).format('YYYY-MM-DD') + " 00:00";
+  }
+  else {
+    start = start.setHours(start.getHours() - 9);
+    start = moment(start).format('YYYY-MM-DD hh:mm');
+  }
+  
+  
+  var end = obj.event._instance.range.end;
+  if(end.getHours() == 9) {
+    end = moment(end).format('YYYY-MM-DD') + " 00:00";
+  }
+  else {
+    end = end.setHours(end.getHours() - 9);
+    end = moment(end).format('YYYY-MM-DD hh:mm');
+  }
+  
+  
+  $.ajax({
+        url: "/~team2/admin/ajax_calendar_edit",
+        type: "POST",
+        data : {
+          title : obj.event._def.title,
+          start: start,
+          end: end
         },
-        initialView: 'dayGridMonth', // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
-        initialDate: '2021-07-15', // 초기 날짜 설정 (설정하지 않으면 오늘 날짜가 보인다.)
-        navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
-        editable: true, // 수정 가능?
-        selectable: true, // 달력 일자 드래그 설정가능
-        nowIndicator: true, // 현재 시간 마크
-        dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
-        locale: 'ko', // 한국어 설정
-        eventAdd: function(obj) { // 이벤트가 추가되면 발생하는 이벤트
-          console.log(obj);
+        traditional: true,
+        async: false, //동기
+        success : function(data){
+          
         },
-        eventChange: function(obj) { // 이벤트가 수정되면 발생하는 이벤트
-          console.log(obj);
-        },
-        eventRemove: function(obj){ // 이벤트가 삭제되면 발생하는 이벤트
-          console.log(obj);
-        },
-        select: function(arg) { // 캘린더에서 드래그로 이벤트를 생성할 수 있다.
-          var title = prompt('Event Title:');
-          if (title) {
-            calendar.addEvent({
-              title: title,
-              start: arg.start,
-              end: arg.end,
-              allDay: arg.allDay
-            })
-          }
-          calendar.unselect()
+        error : function(request,status,error){
+        alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+        console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
         }
-        // 이벤트 
-        events: [
-          {
-            title: 'All Day Event',
-            start: '2021-07-01',
-          },
-          {
-            title: 'Long Event',
-            start: '2021-07-07',
-            end: '2021-07-10'
-          },
-          {
-            groupId: 999,
-            title: 'Repeating Event',
-            start: '2021-07-09T16:00:00'
-          },
-          {
-            groupId: 999,
-            title: 'Repeating Event',
-            start: '2021-07-16T16:00:00'
-          },
-          {
-            title: 'Conference',
-            start: '2021-07-11',
-            end: '2021-07-13'
-          },
-          {
-            title: 'Meeting',
-            start: '2021-07-12T10:30:00',
-            end: '2021-07-12T12:30:00'
-          },
-          {
-            title: 'Lunch',
-            start: '2021-07-12T12:00:00'
-          },
-          {
-            title: 'Meeting',
-            start: '2021-07-12T14:30:00'
-          },
-          {
-            title: 'Happy Hour',
-            start: '2021-07-12T17:30:00'
-          },
-          {
-            title: 'Dinner',
-            start: '2021-07-12T20:00:00'
-          },
-          {
-            title: 'Birthday Party',
-            start: '2021-07-13T07:00:00'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/', // 클릭시 해당 url로 이동
-            start: '2021-07-28'
-          }
-        ]
-      });
-      // 캘린더 랜더링
-      calendar.render();
     });
-  })();
+  
+},
+eventRemove: function(obj){ // 이벤트가 삭제되면 발생하는 이벤트
+  console.log('remove');
+  
+},
+select: function(arg) { // 캘린더에서 드래그로 이벤트 생성
+  
+  var title = prompt('입력할 일정:');
+  if (title) {
+    $.ajax({
+        url: "/~team2/admin/ajax_calendar_add",
+        type: "POST",
+        data : {
+          title: title,
+          start: arg.startStr,
+          end: arg.endStr,
+          allDay: arg.allDay
+        },
+        traditional: true,
+        async: false, //동기
+        success : function(data){
+          
+        },
+        error : function(request,status,error){
+        alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+        console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+        }
+    });
+
+    calendar.addEvent({
+      title: title,
+      start: arg.startStr,
+      end: arg.endStr,
+      allDay: arg.allDay
+    })
+  } 
+  
+  calendar.unselect()
+},
+droppable: true,
+eventClick: function(arg) { 
+  // 있는 일정 클릭시, 
+  console.log(arg);
+  if (confirm('일정을 삭제하시겠습니까?')) 
+  { 
+    $.ajax({
+        url: "/~team2/admin/ajax_calendar_delete",
+        type: "POST",
+        data : {
+          title : arg.event._def.title
+        },
+        traditional: true,
+        async: false, //동기
+        success : function(data){
+         
+        },
+        error : function(request,status,error){
+        alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+        console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+        }
+    });
+    arg.event.remove();
+  } 
+},
+eventBorderColor : '#82d1ff', // 이벤트 테두리색
+eventBackgroundColor : '#82d1ff' , // 이벤트 배경색
+headerToolbar: { // 툴바
+  left: 'prev,next today',
+  center: 'title',
+  right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+},
+events: function(info, successCallback, failureCallback){ // ajax 처리로 데이터를 로딩 시킨다. 
+  $.ajax({
+      url: "/~team2/admin/ajax_calendar_load",
+      type: "POST",
+      dataType: "JSON",
+      traditional: true,
+      async: false, //동기
+      success : function(data){
+        
+      successCallback(data);
+      
+      },
+      error : function(request,status,error){
+      alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+      console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+      }
+  });
+}
+
+  });
+
+  calendar.render(); // 캘린더 렌더링(화면 출력)
+});
+
+/* -------- 캘린더 끝 ----------*/
