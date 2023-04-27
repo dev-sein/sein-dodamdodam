d0af922f7a589a8498b59a17fdd3fa27f4c396b4
dev-peanut <pdy0722@gmail.com>
2023-04-27 19:38:48 +0900

    free-board-detail js 완료

--- src/main/resources/static/js/member/find-password.js
+++ src/main/resources/static/js/member/find-password.js
@@ -5,3 +5,45 @@ function showModal(){
 function closeModal(){
     $('.modal-whole').css('display', 'none');
 }
+
+// 이메일 변수
+const $emailInput = $(".input-text");
+// 이메일 에러 변수
+const $emailWarning = $(".error-text");
+var emailPattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+const $emailInputvalue = $("#memberEmail");
+
+//비밀번호 전송 버튼
+const $submitBtn = $("#submit-btn");
+
+console.log($submitBtn);
+console.log($emailInputvalue);
+console.log($('input[name=memberEmail]').val());
+
+$submitBtn.on("click", function () {
+    if ($emailInput.val()) {
+        $submitBtn.attr("type", "submit");
+    } else {
+        $submitBtn.attr("type", "button");
+    }
+});
+
+
+
+$emailInput.on("blur", function(){
+    /* 이메일 중복 확인 */
+    /* if (result.check) {
+        $emailWarning.text("가입된 계정이 없습니다. 이메일을 다시 확인해주세요.");
+        $emailWarning.css("display", "block");
+        $emailInput.css("border-color", "#e52929");
+        emailFlag = false;
+    }  */if (!emailPattern.test($emailInput.val())) {
+        $emailWarning.text("이메일을 입력해 주세요.");
+        $emailWarning.css("display", "block");
+        $emailInput.css("border-color", "#e52929");
+    } else {
+        $emailWarning.css("display", "none");
+        $emailInput.css("border-color", "#e0e0e0");
+    }
+});
+
