e47c01a7d443bc9340fb32e6c4700ade579a33c7
dev-luk <rladnrtjdsla@gmail.com>
2023-04-27 09:10:51 +0900

    내가쓴 모집, 판매, 내가쓴 게시글 완

---
+++ src/main/java/com/app/dodamdodam/KakaoCalendarExample.java
@@ -0,0 +1,33 @@
+package com.app.dodamdodam;
+
+import okhttp3.*;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+
+public class KakaoCalendarExample {
+    private static final String API_KEY = "c4295049a5b123f27adae92ac07abeb1";
+    private static final String CLIENT_SECRET = "GyfKhVQFtFDkpPPj3YBaPx63BlZnhK5O";
+
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://kapi.kakao.com/v1/calendar/event")
+                .addHeader("Authorization", "KakaoAK " + API_KEY)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                System.out.println(response.body().string());
+            } else {
+                System.out.println("API request failed: " + response.code());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/resources/static/js/myPage/myPage-profile.js
+++ src/main/resources/static/js/myPage/myPage-profile.js
@@ -1,98 +1,51 @@
-// 참여한 요소, 찜한 요소, 게시판 요소를 선택합니다.
 const fundingTab = document.querySelector('.list1');
 const wishTab = document.querySelector('.list2');
 const boardTab = document.querySelector('.list3');
 
-// list2와 list3의 초기 색상을 설정합니다.
-wishTab.style.color = '#90949c';
-boardTab.style.color = '#90949c';
+const projectList1 = document.querySelector('.project-list1');
+const projectList2 = document.querySelector('.project-list2');
+const projectList3 = document.querySelector('.project-list3');
 
-// list1을 클릭하면 실행되는 함수입니다.
+function hideAllLists() {
+  projectList1.style.display = 'none';
+  projectList2.style.display = 'none';
+  projectList3.style.display = 'none';
+}
+
+// 색상 변경 함수 추가
+function resetTabColors() {
+  fundingTab.style.color = '#90949c';
+  wishTab.style.color = '#90949c';
+  boardTab.style.color = '#90949c';
+}
+
 function handleFundingTabClick() {
+  hideAllLists();
+  resetTabColors();
+  projectList1.style.display = 'block';
   fundingTab.style.color = '#00c4c4';
-  wishTab.style.color = '#90949c';
-  boardTab.style.color = '#90949c';
 }
 
-// list2를 클릭하면 실행되는 함수입니다.
 function handleWishTabClick() {
-  fundingTab.style.color = '#90949c';
+  hideAllLists();
+  resetTabColors();
+  projectList2.style.display = 'block';
   wishTab.style.color = '#00c4c4';
-  boardTab.style.color = '#90949c';
 }
 
-// list3를 클릭하면 실행되는 함수입니다.
 function handleBoardTabClick() {
-  fundingTab.style.color = '#90949c';
-  wishTab.style.color = '#90949c';
+  hideAllLists();
+  resetTabColors();
+  projectList3.style.display = 'block';
   boardTab.style.color = '#00c4c4';
 }
 
-// list1, list2, list3에 각각 클릭 이벤트를 추가합니다.
 fundingTab.addEventListener('click', handleFundingTabClick);
 wishTab.addEventListener('click', handleWishTabClick);
 boardTab.addEventListener('click', handleBoardTabClick);
 
-
-/* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ *//* 
-// 참여한 요소, 찜한 요소, 게시판 요소를 선택합니다.
-const fundingTab = document.querySelector('.list1');
-const wishTab = document.querySelector('.list2');
-const boardTab = document.querySelector('.list3');
-
-// 프로젝트 리스트1, 프로젝트 리스트2를 선택합니다.
-const projectList1 = document.querySelector('.project-list1');
-const projectList2 = document.querySelector('.project-list2');
-const projectList3 = documnet.querySelector('.project-list3')
-
-// list2와 list3의 초기 색상을 설정합니다.
-wishTab.style.color = '#90949c';
-boardTab.style.color = '#90949c';
-
-
-// 프로젝트 리스트1은 보이게 하고, 프로젝트 리스트2는 숨깁니다.
+hideAllLists();
 projectList1.style.display = 'block';
-projectList2.style.display = 'none';
-projectList3.style.display ='none' ;
-
-// list1을 클릭하면 실행되는 함수입니다.
-function handleFundingTabClick() {
-    fundingTab.style.color = '#00c4c4';
-    wishTab.style.color = '#90949c';
-    boardTab.style.color = '#90949c';
-
-    // 프로젝트 리스트1은 보이게 하고, 프로젝트 리스트2는 숨깁니다.
-    projectList1.style.display = 'block';
-    projectList2.style.display = 'none';
-    projectList3.style.display ='none' ;
-}
-
-// list2를 클릭하면 실행되는 함수입니다.
-function handleWishTabClick() {
-    fundingTab.style.color = '#90949c';
-    wishTab.style.color = '#00c4c4';
-    boardTab.style.color = '#90949c';
-
-    // 프로젝트 리스트1은 숨기고, 프로젝트 리스트2는 보이게 합니다.
-    projectList1.style.display = 'none';
-    projectList2.style.display = 'block';
-    projectList3.style.display ='none' ;
-}
-
-// list3를 클릭하면 실행되는 함수입니다.
-function handleBoardTabClick() {
-  fundingTab.style.color = '#90949c';
-  wishTab.style.color = '#90949c';
-  boardTab.style.color = '#00c4c4';
-
-   // 프로젝트 리스트1은 숨기고, 프로젝트 리스트2는 보이게 합니다.
-   projectList1.style.display = 'none';
-   projectList2.style.display = 'none';
-   projectList3.style.display ='block' ;
-}
-
-
-// list1과 list2에 각각 클릭 이벤트를 추가합니다.
-fundingTab.addEventListener('click', handleFundingTabClick);
-wishTab.addEventListener('click', handleWishTabClick);
-boardTab.addEventListener('click', handleBoardTabClick); */
+fundingTab.style.color = '#00c4c4';
+wishTab.style.color='#90949c';
+boardTab.style.color='#90949c';
