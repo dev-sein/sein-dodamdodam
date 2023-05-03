cdcf475274bcebc5dea1685420ad25c84872ffaa
dev-luk <rladnrtjdsla@gmail.com>
2023-05-03 16:56:29 +0900

    entity 컬럼 추가

--- src/main/java/com/app/dodamdodam/entity/Room.java
+++ src/main/java/com/app/dodamdodam/entity/Room.java
@@ -1,4 +1,23 @@
 package com.app.dodamdodam.entity;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter @Table(name = "TBL_ROOM")
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Room {
+    @Id @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+    @NotNull
+    private int hostId;
+    @NotNull
+    private int havingId;
 }
