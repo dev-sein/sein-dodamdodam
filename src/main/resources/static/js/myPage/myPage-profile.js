6fde42793efd893f69e67781dd0dbf39ad6ac158
dev-luk <rladnrtjdsla@gmail.com>
2023-05-03 16:39:35 +0900

    프로필 추가 수정

--- src/main/resources/static/js/myPage/myPage-profile.js
+++ src/main/resources/static/js/myPage/myPage-profile.js
@@ -2,17 +2,20 @@ const writeTab = document.querySelector('.list1');
 const collectTab = document.querySelector('.list2');
 const sellTab = document.querySelector('.list3');
 const joinTab = document.querySelector('.list4');
+const eventTab = document.querySelector('.list5');
 
 const projectList1 = document.querySelector('.project-list1');
 const projectList2 = document.querySelector('.project-list2');
 const projectList3 = document.querySelector('.project-list3');
 const projectList4 = document.querySelector('.project-list4');
+const projectList5 = document.querySelector('.project-list5');
 
 function hideAllLists() {
   projectList1.style.display = 'none';
   projectList2.style.display = 'none';
   projectList3.style.display = 'none';
   projectList4.style.display = 'none';
+  projectList5.style.display = 'none';
 }
 
 function resetTabColors() {
@@ -20,6 +23,7 @@ function resetTabColors() {
   collectTab.style.color = '#90949c';
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
+  eventTab.style.color = '#90949c';
 }
 
 function handlewriteTabClick() {
@@ -50,10 +54,19 @@ function handlejoinTabClick() {
   joinTab.style.color = '#006633;';
 }
 
+function handlejoinTabClick() {
+  hideAllLists();
+  resetTabColors();
+  projectList4.style.display = 'block';
+  eventTab.style.color = '#006633;';
+}
+
 writeTab.addEventListener('click', handlewriteTabClick);
 collectTab.addEventListener('click', handlecollectTabClick);
 sellTab.addEventListener('click', handlesellTabClick);
 joinTab.addEventListener('click', handlejoinTabClick);
+eventTab.addEventListener('click', handlejoinTabClick);
+
 
 hideAllLists();
 projectList1.style.display = 'block';
@@ -61,6 +74,8 @@ writeTab.style.color = '#006633;';
 collectTab.style.color='#90949c';
 sellTab.style.color='#90949c';
 joinTab.style.color='#90949c';
+eventTab.style.color='#90949c';
+
 
 // 각 리스트 클릭 시 색상 변경 함수 추가
 writeTab.addEventListener('click', function() {
@@ -68,6 +83,7 @@ writeTab.addEventListener('click', function() {
   collectTab.style.color = '#90949c';
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
+  eventTab.style.color = '#90949c';
 });
 
 collectTab.addEventListener('click', function() {
@@ -75,6 +91,7 @@ collectTab.addEventListener('click', function() {
   collectTab.style.color = '#006633';
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
+  eventTab.style.color = '#90949c';
 });
 
 sellTab.addEventListener('click', function() {
@@ -82,6 +99,7 @@ sellTab.addEventListener('click', function() {
   collectTab.style.color = '#90949c';
   sellTab.style.color = '#006633';
   joinTab.style.color = '#90949c';
+  eventTab.style.color = '#90949c';
 });
 
 joinTab.addEventListener('click', function() {
@@ -89,4 +107,15 @@ joinTab.addEventListener('click', function() {
   collectTab.style.color = '#90949c';
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#006633';
+  eventTab.style.color = '#90949c';
 });
+
+eventTab.addEventListener('click', function() {
+  writeTab.style.color = '#90949c';
+  collectTab.style.color = '#90949c';
+  sellTab.style.color = '#90949c';
+  joinTab.style.color = '#90949c';
+  eventTab.style.color = '#006633';
+});
+
+
