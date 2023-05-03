248b4a92f49944f3984c8d17aa06887cbea057d4
SeyunKimdev <seyun.dev@gmail.com>
2023-05-03 17:05:29 +0900

    김세윤 엔티티 작업 중

--- src/main/java/com/app/dodamdodam/entity/Inquiry.java
+++ src/main/java/com/app/dodamdodam/entity/Inquiry.java
@@ -1,4 +1,25 @@
 package com.app.dodamdodam.entity;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_INQUIRY")
+@Getter @ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Inquiry {
+    @Id @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+    @NotNull private String inquiryType;
+    @NotNull private String inquiryEmail;
+    @NotNull private String memberIdentification;
+    @NotNull private String inquiryPhoneNumber;
+    @NotNull private String inquiryContent;
+    @NotNull private String inquiryAnswer;
 }
