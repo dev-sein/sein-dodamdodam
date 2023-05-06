311c39ff83cc5e227dd59a4b137880179ccf686c
SeyunKimdev <seyun.dev@gmail.com>
2023-05-06 17:01:18 +0900

    김세윤 repository 작업중

---
+++ src/main/java/com/app/dodamdodam/repository/banner/BannerRepository.java
@@ -0,0 +1,8 @@
+package com.app.dodamdodam.repository.banner;
+
+import com.app.dodamdodam.entity.banner.BannerApply;
+import org.springframework.boot.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<BannerApply, Long> {
+}
