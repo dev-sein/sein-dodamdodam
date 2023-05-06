4d0001a865e405f8b6b26387d84452c57db49e6f
dev-mwYoon <dev.mwYoon@gmail.com>
2023-05-06 18:11:40 +0900

    purchase쪽 repository

---
+++ src/main/java/com/app/dodamdodam/repository/board/purchase/PurchaseBoardQueryDsl.java
@@ -0,0 +1,4 @@
+package com.app.dodamdodam.repository.board.purchase;
+
+public interface PurchaseBoardQueryDsl {
+}
---
+++ src/main/java/com/app/dodamdodam/repository/reply/PurchaseReview.java
@@ -0,0 +1,6 @@
+package com.app.dodamdodam.repository.reply;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchaseReview extends JpaRepository<PurchaseReview, Long> {
+}
