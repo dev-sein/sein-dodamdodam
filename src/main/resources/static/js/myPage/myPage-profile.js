554eb70f252cf2a363cbcbb789288bb839eb983d
dev-luk <rladnrtjdsla@gmail.com>
2023-05-07 23:00:00 +0900

    프로필 수정 완료

--- src/main/resources/static/js/myPage/myPage-profile.js
+++ src/main/resources/static/js/myPage/myPage-profile.js
@@ -3,12 +3,14 @@ const collectTab = document.querySelector('.list2');
 const sellTab = document.querySelector('.list3');
 const joinTab = document.querySelector('.list4');
 const eventTab = document.querySelector('.list5');
+const buyTab = document.querySelector('.list6');
 
 const projectList1 = document.querySelector('.project-list1');
 const projectList2 = document.querySelector('.project-list2');
 const projectList3 = document.querySelector('.project-list3');
 const projectList4 = document.querySelector('.project-list4');
 const projectList5 = document.querySelector('.project-list5');
+const projectList6 = document.querySelector('.project-list6');
 
 function hideAllLists() {
   projectList1.style.display = 'none';
@@ -16,6 +18,7 @@ function hideAllLists() {
   projectList3.style.display = 'none';
   projectList4.style.display = 'none';
   projectList5.style.display = 'none';
+  projectList6.style.display = 'none';
 }
 
 function resetTabColors() {
@@ -24,6 +27,7 @@ function resetTabColors() {
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
   eventTab.style.color = '#90949c';
+  buyTab.style.color = '#90949c';
 }
 
 function handlewriteTabClick() {
@@ -54,18 +58,26 @@ function handlejoinTabClick() {
   joinTab.style.color = '#006633;';
 }
 
-function handlejoinTabClick() {
+function handleEventTabClick() {
   hideAllLists();
   resetTabColors();
-  projectList4.style.display = 'block';
+  projectList5.style.display = 'block';
   eventTab.style.color = '#006633;';
+}
+
+function handlebuyTabClick() {
+  hideAllLists();
+  resetTabColors();
+  projectList6.style.display = 'block';
+  buyTab.style.color = '#006633;';
 }
 
 writeTab.addEventListener('click', handlewriteTabClick);
 collectTab.addEventListener('click', handlecollectTabClick);
 sellTab.addEventListener('click', handlesellTabClick);
 joinTab.addEventListener('click', handlejoinTabClick);
-eventTab.addEventListener('click', handlejoinTabClick);
+eventTab.addEventListener('click', handleEventTabClick);
+buyTab.addEventListener('click', handlebuyTabClick);
 
 
 hideAllLists();
@@ -75,6 +87,7 @@ collectTab.style.color='#90949c';
 sellTab.style.color='#90949c';
 joinTab.style.color='#90949c';
 eventTab.style.color='#90949c';
+buyTab.style.color='#90949c';
 
 
 // 각 리스트 클릭 시 색상 변경 함수 추가
@@ -84,6 +97,7 @@ writeTab.addEventListener('click', function() {
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
   eventTab.style.color = '#90949c';
+  buytTab.style.color = '#90949c';
 });
 
 collectTab.addEventListener('click', function() {
@@ -92,6 +106,7 @@ collectTab.addEventListener('click', function() {
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
   eventTab.style.color = '#90949c';
+  buytTab.style.color = '#90949c';
 });
 
 sellTab.addEventListener('click', function() {
@@ -100,6 +115,7 @@ sellTab.addEventListener('click', function() {
   sellTab.style.color = '#006633';
   joinTab.style.color = '#90949c';
   eventTab.style.color = '#90949c';
+  buytTab.style.color = '#90949c';
 });
 
 joinTab.addEventListener('click', function() {
@@ -108,6 +124,7 @@ joinTab.addEventListener('click', function() {
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#006633';
   eventTab.style.color = '#90949c';
+  buytTab.style.color = '#90949c';
 });
 
 eventTab.addEventListener('click', function() {
@@ -116,6 +133,57 @@ eventTab.addEventListener('click', function() {
   sellTab.style.color = '#90949c';
   joinTab.style.color = '#90949c';
   eventTab.style.color = '#006633';
+  buytTab.style.color = '#90949c';
 });
+
+buyTab.addEventListener('click', function() {
+  writeTab.style.color = '#90949c';
+  collectTab.style.color = '#90949c';
+  sellTab.style.color = '#90949c';
+  joinTab.style.color = '#90949c';
+  eventTab.style.color = '#90949c';
+  buyTab.style.color = '#006633';
+});
+
+
+/* 모달창 열기 */
+const modal = document.getElementById("myModal");
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.getElementsByClassName("close")[0];
+
+// 모달창을 연다.
+openModalButton.onclick = function () {
+  modal.style.display = "block";
+};
+
+// 모달창을 닫는다.
+closeModalButton.onclick = function () {
+  modal.style.display = "none";
+};
+
+// 모달창 외부를 클릭하면 모달창을 닫는다.
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+/* 링크  */
+const openChatButton = document.querySelector('.management2');
+const tooltip = document.querySelector('.tooltip');
+
+function toggleTooltip() {
+  if (tooltip.style.display === 'none') {
+    tooltip.style.display = 'block';
+    openChatButton.style.display = 'none';
+  } else {
+    tooltip.style.display = 'none';
+    openChatButton.style.display = 'block';
+  }
+}
+
+openChatButton.addEventListener('click', toggleTooltip);
+
 
 
