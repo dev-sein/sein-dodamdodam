8f4170e4bb019a7449842ffeac7e98b86d5e6948
dev-luk <rladnrtjdsla@gmail.com>
2023-05-08 15:18:38 +0900

    이벤트 레포지토리 작성

--- src/main/java/com/app/dodamdodam/repository/board/event/EventBoardQueryDsl.java
+++ src/main/java/com/app/dodamdodam/repository/board/event/EventBoardQueryDsl.java
@@ -1,18 +1,12 @@
 package com.app.dodamdodam.repository.board.event;
 
-import com.app.dodamdodam.entity.purchase.PurchaseBoard;
+import com.app.dodamdodam.entity.event.EventBoard;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EventBoardQueryDsl {
-    //    무한스크롤 10개씩
-    Slice<PurchaseBoard> findAllEventBoard(Pageable pageable);
-    //    판매게시글 각각
-    Optional<PurchaseBoard> findEventBoardById(Long id);
 
-    //    세션에 담긴 id 값 받아와서 내가 작성한 자유 게시글 리스트 가져오기
-    public List<PurchaseBoard> findEventBoardListByMemberId(Pageable pageable, Long memberId);
+    //    세션에 담긴 id 값 받아와서 내가 작성한 이벤트 리스트 가져오기
+    public List<EventBoard> findEventBoardListByMemberId(Pageable pageable, Long memberId);
 }
