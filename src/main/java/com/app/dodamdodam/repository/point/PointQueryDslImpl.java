2d40972e43909b8362e91229fa7a34912e506f3d
dev-sein <seinee114@gmail.com>
2023-05-10 14:55:32 +0900

    관리자 단위 테스트 진행중

--- src/main/java/com/app/dodamdodam/repository/point/PointQueryDslImpl.java
+++ src/main/java/com/app/dodamdodam/repository/point/PointQueryDslImpl.java
@@ -1,10 +1,12 @@
 package com.app.dodamdodam.repository.point;
 
-import com.app.dodamdodam.entity.free.FreeBoard;
+import com.app.dodamdodam.entity.member.QGrade;
+import com.app.dodamdodam.entity.member.QMember;
 import com.app.dodamdodam.entity.point.Point;
-import com.app.dodamdodam.search.point.PointSearch;
+import com.app.dodamdodam.search.point.AdminPointSearch;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,10 +15,11 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static com.app.dodamdodam.entity.free.QFreeBoard.freeBoard;
+import static com.app.dodamdodam.entity.member.QMember.member;
 import static com.app.dodamdodam.entity.point.QPoint.point;
 
 @Slf4j
+@RequiredArgsConstructor
 public class PointQueryDslImpl implements PointQueryDsl {
     @Autowired
     private JPAQueryFactory query;
@@ -27,15 +30,16 @@ public class PointQueryDslImpl implements PointQueryDsl {
     }
 
     @Override
-    public Page<Point> findAllPointWithSearch(PointSearch pointSearch, Pageable pageable) {
+    public Page<Point> findPointWithSearch(AdminPointSearch pointSearch, Pageable pageable) {
         BooleanExpression pointAmountEq = pointSearch.getPointAmount() == null ? null : point.pointAmount.eq(pointSearch.getPointAmount());
         BooleanExpression pointStatusEq = pointSearch.getPointStatus() == null ? null : point.pointStatus.eq(pointSearch.getPointStatus());
+
 
         List<Point> points = query.select(point)
                 .from(point)
                 .where(pointAmountEq, pointStatusEq)
                 .orderBy(point.id.desc())
-                .offset(pageable.getOffset() - 1)
+                .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
@@ -46,4 +50,30 @@ public class PointQueryDslImpl implements PointQueryDsl {
 
         return new PageImpl<>(points, pageable, count);
     }
+
+
+    @Override
+    public Page<Point> findPointMemberIdWithSearch(AdminPointSearch pointSearch, Pageable pageable) {
+        BooleanExpression memberIdEq = pointSearch.getMemberId() == null ? null : member.memberId.eq(pointSearch.getMemberId());
+      //  BooleanExpression memberNameEq = pointSearch.get() == null ? null : member.memberId.eq(pointSearch.getMemberId());
+
+        List<Point> points = query.select(point)
+                .from(point)
+                .join(point.member)
+                .fetchJoin()
+                .where(memberIdEq)
+                .orderBy(point.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    //fetchJoin일 때 한 사람, join 할 때
+        log.info(pageable.getOffset()+"/");
+        log.info(pageable.getPageSize()+"/");
+        log.info(points.toString());
+        Long count = query.select(point.count()).from(point).fetchOne();
+
+        return new PageImpl<>(points, pageable, count);
+    }
+
+
 }
