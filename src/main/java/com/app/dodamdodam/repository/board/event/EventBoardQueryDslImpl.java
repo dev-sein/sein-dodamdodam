e7540d68d03b4aafefae2a1a85a9a9f4044cb3c4
dev-peanut <pdy0722@gmail.com>
2023-05-12 08:53:23 +0900

    room 검색

--- src/main/java/com/app/dodamdodam/repository/board/event/EventBoardQueryDslImpl.java
+++ src/main/java/com/app/dodamdodam/repository/board/event/EventBoardQueryDslImpl.java
@@ -2,9 +2,7 @@ package com.app.dodamdodam.repository.board.event;
 
 import com.app.dodamdodam.domain.EventBoardDTO;
 import com.app.dodamdodam.domain.QEventBoardDTO;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.hibernate.Criteria;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
