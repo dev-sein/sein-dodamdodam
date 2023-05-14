3e786398453bf15d180e6b62d14f4a92969a8223
dev-sein <seinee114@gmail.com>
2023-05-15 02:22:34 +0900

    관리자 문의, 포인트 수정중

--- src/main/java/com/app/dodamdodam/repository/point/PointQueryDsl.java
+++ src/main/java/com/app/dodamdodam/repository/point/PointQueryDsl.java
@@ -1,5 +1,6 @@
 package com.app.dodamdodam.repository.point;
 
+import com.app.dodamdodam.entity.inquiry.Inquiry;
 import com.app.dodamdodam.entity.point.Point;
 import com.app.dodamdodam.search.point.AdminPointSearch;
 import org.springframework.data.domain.Page;
@@ -10,6 +11,9 @@ import java.util.List;
 public interface PointQueryDsl {
 //    memberId로 point 내역 조회
     public List<Point> findPointByMemberId_QueryDSL(Long memberId);
+
+    //문의사항 관리자 페이징
+    public Page<Point> findAllWithPaging(Pageable pageable);
 
 //  어드민 포인트 정보 검색
     public Page<Point> findPointMemberIdWithSearch_QueryDSL(AdminPointSearch pointSearch, Pageable pageable);
