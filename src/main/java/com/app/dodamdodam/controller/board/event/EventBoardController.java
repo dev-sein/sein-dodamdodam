b8c272b0de2c2a6583a9f05dfe01fd7b7b838ef2
dev-luk <rladnrtjdsla@gmail.com>
2023-05-15 10:38:42 +0900

    캘린더 완료

--- src/main/java/com/app/dodamdodam/controller/board/event/EventBoardController.java
+++ src/main/java/com/app/dodamdodam/controller/board/event/EventBoardController.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Controller
 @RequestMapping("/board/event/*")
