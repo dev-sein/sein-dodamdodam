5a100e86186a5992925e7b80bb67291b2916699d
dev-luk <rladnrtjdsla@gmail.com>
2023-05-16 09:14:44 +0900

    오류 수정

--- src/main/java/com/app/dodamdodam/domain/EventBoardReviewDTO.java
+++ src/main/java/com/app/dodamdodam/domain/EventBoardReviewDTO.java
@@ -1,9 +1,11 @@
 package com.app.dodamdodam.domain;
 
+import com.app.dodamdodam.entity.event.EventReview;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class EventBoardReviewDTO {
     private Long id;
-    private MemberDTO memberDTO;
+    private String memberName;
     private String replyContent;
+    private String gradeTitle;
 
+
+
+    public EventReview toEntity(){
+        return EventReview.builder()
+                .replyContent(replyContent)
+                .build();
+    }
 }
