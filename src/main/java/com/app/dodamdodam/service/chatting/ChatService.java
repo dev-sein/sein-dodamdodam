177ad94629450328d320492288ae2c2eb87aadff
dev-peanut <pdy0722@gmail.com>
2023-05-17 23:41:32 +0900

    수정

--- src/main/java/com/app/dodamdodam/service/chatting/ChatService.java
+++ src/main/java/com/app/dodamdodam/service/chatting/ChatService.java
@@ -114,8 +114,8 @@ public interface ChatService {
                 .memberStatus(member.getMemberStatus())
                 .memberPoint(member.getMemberPoint())
                 .participationCount(member.getParticipationCount())
-                .recruitmentedCount(member.getParticipationCount())
-                .address(member.getAddress())
+                .address(member.getAddress().getAddress())
+                .addressDetail(member.getAddress().getAddressDetail())
                 .build();
     }
 
