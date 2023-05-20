63804abf4fd0add8dfcfc0be8ee8027cb258c044
dev-peanut <pdy0722@gmail.com>
2023-05-21 02:12:08 +0900

    채팅 기본적인 적용 완료

---
+++ src/main/java/com/app/dodamdodam/domain/chat/ChatMessageDTO.java
@@ -0,0 +1,28 @@
+package com.app.dodamdodam.domain.chat;
+
+import com.app.dodamdodam.type.MessageType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ChatMessageDTO {
+
+    private MessageType type; //메시지 타입
+    private String roomId;// 방 번호
+    private String sender;//채팅을 보낸 사람
+    private String message;// 메세지
+    private String time; // 채팅 발송 시간
+
+    @Builder
+    public ChatMessageDTO(MessageType type, String roomId, String sender, String message, String time) {
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+    }
+}
--- src/main/java/com/app/dodamdodam/repository/room/RoomQueryDslImpl.java
+++ src/main/java/com/app/dodamdodam/repository/room/RoomQueryDslImpl.java
@@ -1,92 +1,92 @@
-package com.app.dodamdodam.repository.room;
-
-import com.app.dodamdodam.entity.chatting.QChatting;
-import com.app.dodamdodam.entity.chatting.Room;
-import com.app.dodamdodam.search.chatting.RoomSearch;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
-
-import java.util.List;
-
-import static com.app.dodamdodam.entity.chatting.QChatting.chatting;
-import static com.app.dodamdodam.entity.chatting.QRoom.room;
-import static com.app.dodamdodam.entity.free.QFreeBoard.freeBoard;
-
-
-public class RoomQueryDslImpl implements RoomQueryDsl{
-    @Autowired
-    private JPAQueryFactory query;
-
-
+//package com.app.dodamdodam.repository.room;
+//
+//import com.app.dodamdodam.entity.chatting.QChatting;
+//import com.app.dodamdodam.entity.chatting.Room;
+//import com.app.dodamdodam.search.chatting.RoomSearch;
+//import com.querydsl.core.types.dsl.BooleanExpression;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.*;
+//
+//import java.util.List;
+//
+//import static com.app.dodamdodam.entity.chatting.QChatting.chatting;
+//import static com.app.dodamdodam.entity.chatting.QRoom.room;
+//import static com.app.dodamdodam.entity.free.QFreeBoard.freeBoard;
+//
+//
+//public class RoomQueryDslImpl implements RoomQueryDsl{
+//    @Autowired
+//    private JPAQueryFactory query;
+//
+//
+////    @Override
+////    public List<Room> findRoomByMemberId(Pageable pageable, Long memberId) {
+////        return query.select(room)
+////                .from(room)
+////                .join(room.chattings)
+////                .fetchJoin()
+////                .where(room.member.id.eq(memberId))
+////                .orderBy(room.id.desc())
+////                .offset(pageable.getOffset())
+////                .limit(pageable.getPageSize())
+////                .fetch();
+////    }
+//
+//
+////    룸 목록 무한스크롤
+////    세션으로 넘겨받은 MEMBERID를 HOSTID와 같은지 조회 해서 ROOM 목록을 불러온다.
 //    @Override
-//    public List<Room> findRoomByMemberId(Pageable pageable, Long memberId) {
-//        return query.select(room)
+//    public Page<Room> findRoomByMemberId_QueryDSL(Pageable pageable, Long memberId) {
+//        List<Room> rooms = query.select(room)
 //                .from(room)
-//                .join(room.chattings)
-//                .fetchJoin()
-//                .where(room.member.id.eq(memberId))
+//                .where(room.hostId.eq(memberId))
+//                .join(room.chattings).fetchJoin()
 //                .orderBy(room.id.desc())
 //                .offset(pageable.getOffset())
 //                .limit(pageable.getPageSize())
 //                .fetch();
+//        Long count = query.select(room.count()).from(room)
+//                .where(room.hostId.eq(memberId)).fetchOne();
+//
+//        return new PageImpl<>(rooms, pageable, count);
 //    }
-
-
-//    룸 목록 무한스크롤
-//    세션으로 넘겨받은 MEMBERID를 HOSTID와 같은지 조회 해서 ROOM 목록을 불러온다.
-    @Override
-    public Page<Room> findRoomByMemberId_QueryDSL(Pageable pageable, Long memberId) {
-        List<Room> rooms = query.select(room)
-                .from(room)
-                .where(room.hostId.eq(memberId))
-                .join(room.chattings).fetchJoin()
-                .orderBy(room.id.desc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-        Long count = query.select(room.count()).from(room)
-                .where(room.hostId.eq(memberId)).fetchOne();
-
-        return new PageImpl<>(rooms, pageable, count);
-    }
-
-//    현재 시퀀스 조회
-    @Override
-    public Room getCurrentSequence() {
-        return query.select(room)
-                .from(room)
-                .orderBy(room.id.desc())
-                .limit(1)
-                .fetchOne();
-    }
-
-//    검색 무한스크롤
-    @Override
-    public Slice<Room> findRoomSearchWithPaging_QueryDSL(RoomSearch roomSearch, Pageable pageable) {
-        BooleanExpression memberNameContains = roomSearch.getMemberName() == null ? null : room.member.memberName.contains(roomSearch.getMemberName());
-        BooleanExpression chattingContentContains = roomSearch.getChattingContent() == null ? null : room.chattings.get(0).chattingContent.contains(roomSearch.getChattingContent());
-
-        boolean hasNext = false;
-
-        List<Room> rooms = query.select(room)
-                .from(room)
-                .join(room.chattings).fetchJoin()
-                .join(room.member).fetchJoin()
-                .where(memberNameContains, chattingContentContains)
-                .orderBy(room.id.desc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize() + 1)
-                .fetch();
-
-        if(rooms.size() > pageable.getPageSize()) {
-            hasNext = true;
-            rooms.remove(pageable.getPageSize()); // 한개 더 가져왔으니 더 가져온 데이터 삭제
-        }
-
-        return new SliceImpl<>(rooms, pageable, hasNext);
-    }
-
-
-}
+//
+////    현재 시퀀스 조회
+//    @Override
+//    public Room getCurrentSequence() {
+//        return query.select(room)
+//                .from(room)
+//                .orderBy(room.id.desc())
+//                .limit(1)
+//                .fetchOne();
+//    }
+//
+////    검색 무한스크롤
+//    @Override
+//    public Slice<Room> findRoomSearchWithPaging_QueryDSL(RoomSearch roomSearch, Pageable pageable) {
+//        BooleanExpression memberNameContains = roomSearch.getMemberName() == null ? null : room.member.memberName.contains(roomSearch.getMemberName());
+//        BooleanExpression chattingContentContains = roomSearch.getChattingContent() == null ? null : room.chattings.get(0).chattingContent.contains(roomSearch.getChattingContent());
+//
+//        boolean hasNext = false;
+//
+//        List<Room> rooms = query.select(room)
+//                .from(room)
+//                .join(room.chattings).fetchJoin()
+//                .join(room.member).fetchJoin()
+//                .where(memberNameContains, chattingContentContains)
+//                .orderBy(room.id.desc())
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize() + 1)
+//                .fetch();
+//
+//        if(rooms.size() > pageable.getPageSize()) {
+//            hasNext = true;
+//            rooms.remove(pageable.getPageSize()); // 한개 더 가져왔으니 더 가져온 데이터 삭제
+//        }
+//
+//        return new SliceImpl<>(rooms, pageable, hasNext);
+//    }
+//
+//
+//}
