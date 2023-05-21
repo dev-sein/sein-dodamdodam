e4234d0ae526b00c74a736fc38c31f30e1de44d4
dev-sein <seinee114@gmail.com>
2023-05-21 16:23:41 +0900

    헤더, 푸터, 메인 수정중

--- src/main/java/com/app/dodamdodam/controller/PurchaseBoardController.java
+++ src/main/java/com/app/dodamdodam/controller/PurchaseBoardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/boards/purchase/*")
+    @RequestMapping("/boards/purchase/*")
 @RequiredArgsConstructor
 public class PurchaseBoardController {
     private final PurchaseBoardService purchaseBoardService;
